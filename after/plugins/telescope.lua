68815d5f9ab9212e67fdd4df73a65daeb6895dfb
zantpok <anton.pokhodun.ext@ericsson.com>
2024-01-21 17:59:10 +0200

    Initial commit

---
+++ after/plugins/telescope.lua
@@ -0,0 +1,10 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+--To make it work you might need to install
+--brew install ripgrep
+vim.keymap.set('n', '<leader>fs', function() 
+  builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end);
