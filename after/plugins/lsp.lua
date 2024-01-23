2eff68fd5206d95f442c5f47c94567bba5bbd889
zantpok <anton.pokhodun.ext@ericsson.com>
2024-01-23 09:13:50 +0200

    Added some keybindings

--- after/plugins/lsp.lua
+++ after/plugins/lsp.lua
@@ -1,7 +1,13 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(_, bufnr)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr, desc = "Go to the definition" })
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP Hover" })
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = bufnr, desc = "Go to Implementation" })
+    vim.keymap.set("n", "<cmd>[", vim.diagnostic.goto_prev, { buffer = bufnr, desc = "Go to Next Diagnostic" })
+    vim.keymap.set("n", "gl", vim.diagnostic.open_float, { buffer = bufnr, desc = "Open Diagnostic Float" })
+    vim.keymap.set("n", "<cmd>[", vim.diagnostic.goto_next, { buffer = bufnr, desc = "Go to Previous Diagnostic" })
+
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
   lsp_zero.default_keymaps({buffer = bufnr})
@@ -10,6 +16,7 @@ end)
 -- to learn how to use mason.nvim with lsp-zero
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
+
 require('mason-lspconfig').setup({
   ensure_installed = { 'lua_ls' },
   handlers = {
