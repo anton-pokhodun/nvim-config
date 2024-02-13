e95ebfeea7d5c23e205121ee74c9406bf34e7dce
zantpok <anton.pokhodun.ext@ericsson.com>
2024-02-13 20:58:39 +0200

    Set mapping for telescope preview window navigation. Customized search layout.

--- after/plugins/telescope.lua
+++ after/plugins/telescope.lua
@@ -1,13 +1,47 @@
 local telescope = require('telescope')
 local lga_actions = require("telescope-live-grep-args.actions")
 local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
 
 telescope.setup {
+        -- mappings = {
+        --     i = {
+        --         ["<C-k>"] = actions.move_selection_previous,
+        --         ["<C-j>"] = actions.move_selection_next,
+        --         ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+        --     }
+        -- },
         defaults = {
+            mappings = {
+                i = {
+                    ['<C-k>'] = actions.preview_scrolling_up,
+                    ['<C-j>'] = actions.preview_scrolling_down
+                }
+            },
             hidden = true,
+            sorting_strategy = "ascending",
+            layout_strategy = "horizontal",
             no_ignore = false,
             show_preview = true,
             file_ignore_patterns = {".git/", ".cache", "node_modules/.*", "packer_compiled.lua"},
+            layout_config = {
+                horizontal = {
+                    prompt_position = "top",
+                    preview_width = 0.55,
+                    results_width = 0.8,
+                },
+                vertical = {
+                    mirror = false,
+                },
+                width = 0.87,
+                height = 0.80,
+                preview_cutoff = 120,
+            },
+            border = {},
+
+        },
+        pickers = {
+            current_buffer_fuzzy_find = { sorting_strategy = 'descending' },
         },
         extensions = {
             live_grep_args = {
@@ -16,7 +50,7 @@ telescope.setup {
                 mappings = { -- extend mappings
                 i = {
                     ["<C-k>"] = lga_actions.quote_prompt(),
-                    ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                    ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " })
                 },
             },
         }
