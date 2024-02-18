57f0b9996c33075f90e6e30b1ccf5e14057c9b32
zantpok <anton.pokhodun.ext@ericsson.com>
2024-02-18 16:06:57 +0200

    Configured lsp for ruby. Requires version >= 3.0.

--- after/plugins/lsp.lua
+++ after/plugins/lsp.lua
@@ -1,19 +1,36 @@
 local lsp_zero = require('lsp-zero')
+local lspconfig = require('lspconfig')
+local util = require("lspconfig.util")
+local configs = require("lspconfig.configs")
 
-lsp_zero.on_attach(function(_, bufnr)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr, desc = "Go to the definition" })
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP Hover" })
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = bufnr, desc = "Go to Implementation" })
-    vim.keymap.set("n", "gl", vim.diagnostic.open_float, { buffer = bufnr, desc = "Open Diagnostic Float" })
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { buffer = bufnr, desc = "Go to Next Diagnostic" })
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { buffer = bufnr, desc = "Go to Previous Diagnostic" })
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
+-- vim.api.nvim_create_autocmd('LspAttach', {
+  -- group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  -- callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    -- vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
-end)
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    -- local opts = { buffer = ev.buf }
+    -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    -- vim.keymap.set('n', '<leader>s', vim.lsp.buf.signature_help, opts)
+    -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    -- vim.keymap.set('n', '<space>wl', function()
+    --     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    -- end, opts)
+    -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    -- vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    -- vim.keymap.set('n', '<space>f', function()
+    --     vim.lsp.buf.format { async = true }
+    -- end, opts)  
+-- end,
+-- })
 
 -- to learn how to use mason.nvim with lsp-zero
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
@@ -30,11 +47,50 @@ require('lspconfig').dockerls.setup({})
 
 require('lspconfig').marksman.setup({})
 
-require('lspconfig').solargraph.setup({})
+if not configs.ruby_ls then
+    local enabled_features = {
+        "documentHighlights",
+        "documentSymbols",
+        "foldingRanges",
+        "selectionRanges",
+        "semanticHighlighting",
+        "formatting",
+        "codeActions",
+    }
 
-require('lspconfig').yamlls.setup({})
+    configs.ruby_ls = {
+        default_config = {
+            single_file_support = true,
+            cmd = { "bundle", "exec", "ruby-lsp" },
+            filetypes = { "ruby" },
+            root_dir = util.root_pattern("Gemfile", ".git"),
+            init_options = {
+                -- enabledFeatures = enabled_features,
+                formatter = "auto",
+            },
+            settings = {},
+        },
+        commands = {
+            FormatRuby = {
+                function()
+                    vim.lsp.buf.format({
+                        name = "ruby_ls",
+                        async = true,
+                    })
+                end,
+                description = "Format using ruby-lsp",
+            },
+        },
+    }
+end
 
-require('lspconfig').lua_ls.setup {
+lspconfig.ruby_ls.setup({})
+
+lspconfig.solargraph.setup({})
+
+lspconfig.yamlls.setup({})
+
+lspconfig.lua_ls.setup {
     on_attach = lsp_zero.on_attach,
     settings = {
         Lua = {
