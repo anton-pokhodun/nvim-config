70dc7bbf266eec9ab2111a42f4513a5e56e6f142
zantpok <anton.pokhodun.ext@ericsson.com>
2024-02-19 12:34:05 +0200

    Set lsp for ruby

--- after/plugins/lsp.lua
+++ after/plugins/lsp.lua
@@ -3,88 +3,74 @@ local lspconfig = require('lspconfig')
 local util = require("lspconfig.util")
 local configs = require("lspconfig.configs")
 
--- vim.api.nvim_create_autocmd('LspAttach', {
-  -- group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  -- callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    -- vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    -- local opts = { buffer = ev.buf }
-    -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    -- vim.keymap.set('n', '<leader>s', vim.lsp.buf.signature_help, opts)
-    -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    -- vim.keymap.set('n', '<space>wl', function()
-    --     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    -- end, opts)
-    -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    -- vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    -- vim.keymap.set('n', '<space>f', function()
-    --     vim.lsp.buf.format { async = true }
-    -- end, opts)  
--- end,
--- })
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gd', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gb', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', '<leader>s', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set('n', '<space>d', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+    end,
+})
 
--- to learn how to use mason.nvim with lsp-zero
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
 
 require('mason-lspconfig').setup({
-  ensure_installed = { 'lua_ls', 'ruby_ls', 'bashls', 'solargraph', 'marksman', 'dockerls', 'yamlls' },
-  handlers = {
-    lsp_zero.default_setup,
-  },
+    ensure_installed = { 'lua_ls', 'ruby_ls', 'bashls', 'solargraph', 'marksman', 'dockerls', 'yamlls' },
+    handlers = {
+        lsp_zero.default_setup,
+    },
 })
 
 require('lspconfig').dockerls.setup({})
 
 require('lspconfig').marksman.setup({})
 
-if not configs.ruby_ls then
-    local enabled_features = {
-        "documentHighlights",
-        "documentSymbols",
-        "foldingRanges",
-        "selectionRanges",
-        "semanticHighlighting",
-        "formatting",
-        "codeActions",
-    }
-
-    configs.ruby_ls = {
-        default_config = {
-            single_file_support = true,
-            cmd = { "bundle", "exec", "ruby-lsp" },
-            filetypes = { "ruby" },
-            root_dir = util.root_pattern("Gemfile", ".git"),
-            init_options = {
-                -- enabledFeatures = enabled_features,
-                formatter = "auto",
-            },
-            settings = {},
+require('lspconfig').ruby_ls.setup({
+    default_config = {
+        single_file_support = true,
+        cmd = { "bundle", "exec", "ruby-lsp" },
+        filetypes = { "ruby" },
+        root_dir = util.root_pattern("Gemfile", ".git"),
+        init_options = {
+            -- enabledFeatures = enabled_features,
+            formatter = "auto",
         },
-        commands = {
-            FormatRuby = {
-                function()
-                    vim.lsp.buf.format({
-                        name = "ruby_ls",
-                        async = true,
-                    })
-                end,
-                description = "Format using ruby-lsp",
-            },
+        settings = {},
+    },
+    commands = {
+        FormatRuby = {
+            function()
+                vim.lsp.buf.format({
+                    name = "ruby_ls",
+                    async = true,
+                })
+            end,
+            description = "Format using ruby-lsp",
         },
-    }
-end
+    },
 
-lspconfig.ruby_ls.setup({})
+})
 
 lspconfig.solargraph.setup({})
 
--- lua/hpkns/packer.lua
+++ lua/hpkns/packer.lua
@@ -5,126 +5,131 @@ vim.g.mapleader = " "
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
-    requires = {
-        'nvim-lua/plenary.nvim',
-        'nvim-telescope/telescope-live-grep-args.nvim',
-        'sharkdp/fd'
-    },
-    config = function()
-        require("telescope").load_extension("live_grep_args")
-    end
-  }
-  use({
-      "princejoogie/dir-telescope.nvim",
-      -- telescope.nvim is a required dependency
-      requires = {"nvim-telescope/telescope.nvim"},
-  })
-  use {
-      'crusj/bookmarks.nvim',
-      branch = 'main',
-      requires = { 'kyazdani42/nvim-web-devicons' },
-      config = function()
-          require("bookmarks").setup()
-          require("telescope").load_extension("bookmarks")
-      end
-  }
-  use {
-      "folke/tokyonight.nvim",
-      lazy = false,
-      priority = 1000,
-      opts = {},
-  }
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
-  use('theprimeagen/harpoon')
-  use('mbbill/undotree')
-  use('tpope/vim-fugitive')
-  use('vim-airline/vim-airline')
-  use('vim-airline/vim-airline-themes')
-  use {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "neovim/nvim-lspconfig",
-  }
-  use {
-      'VonHeikemen/lsp-zero.nvim',
-      branch = 'v3.x',
-      requires = {
-          --- Uncomment the two plugins below if you want to manage the language servers from neovim
-          -- {'williamboman/mason.nvim'},
-          -- {'williamboman/mason-lspconfig.nvim'},
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        requires = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope-live-grep-args.nvim',
+            'sharkdp/fd'
+        },
+        config = function()
+            require("telescope").load_extension("live_grep_args")
+        end
+    }
+    use({
+        "princejoogie/dir-telescope.nvim",
+        -- telescope.nvim is a required dependency
+        requires = { "nvim-telescope/telescope.nvim" },
+    })
+    use {
+        'crusj/bookmarks.nvim',
+        branch = 'main',
+        requires = { 'kyazdani42/nvim-web-devicons' },
+        config = function()
+            require("bookmarks").setup()
+            require("telescope").load_extension("bookmarks")
+        end
+    }
+    use {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    }
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/playground')
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
+    use('vim-airline/vim-airline')
+    use('vim-airline/vim-airline-themes')
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-vsnip",
+        "hrsh7th/vim-vsnip",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        -- "SirVer/ultisnips",
+        -- "quannguyen30192/cmp-nvim-ultisnips",
+        "dcampos/nvim-snippy",
+        "dcampos/cmp-snippy"
+    }
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment the two plugins below if you want to manage the language servers from neovim
+            -- {'williamboman/mason.nvim'},
+            -- {'williamboman/mason-lspconfig.nvim'},
 
-          -- LSP Support
-          {'neovim/nvim-lspconfig'},
-          -- Autocompletion
-          {'hrsh7th/nvim-cmp'},
-          {'hrsh7th/cmp-nvim-lsp'},
-          {'L3MON4D3/LuaSnip'},
-      }
-  }
-  use {
-      "AckslD/nvim-neoclip.lua",
-      requires = {
-          {'kkharji/sqlite.lua', module = 'sqlite'},
-          -- you'll need at least one of these
-          -- {'nvim-telescope/telescope.nvim'},
-          -- {'ibhagwan/fzf-lua'},
-      },
-      config = function()
-          require('neoclip').setup()
-      end,
-  }
-  use {
-      'lewis6991/gitsigns.nvim',
-      requires = { 'nvim-lua/plenary.nvim' },
-      config = function()
-          require('gitsigns').setup { current_line_blame = true }
-      end
-  }
-  use {'ThePrimeagen/git-worktree.nvim'}
-  use {
-      "terrortylor/nvim-comment",
-      cmd = "CommentToggle",
-      config = function()
-          require("nvim_comment").setup()
-      end
-  }
-  -- Nerdtree and plugins
-  use('preservim/nerdtree')
-  use('ryanoasis/vim-devicons')
-  use('Xuyuanp/nerdtree-git-plugin')
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'L3MON4D3/LuaSnip' },
+        }
+    }
+    use {
+        "AckslD/nvim-neoclip.lua",
+        requires = {
+            { 'kkharji/sqlite.lua', module = 'sqlite' },
+            -- you'll need at least one of these
+            -- {'nvim-telescope/telescope.nvim'},
+            -- {'ibhagwan/fzf-lua'},
+        },
+        config = function()
+            require('neoclip').setup()
+        end,
+    }
+    use {
+        'lewis6991/gitsigns.nvim',
+        requires = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            require('gitsigns').setup { current_line_blame = true }
+        end
+    }
+    use { 'ThePrimeagen/git-worktree.nvim' }
+    use {
+        "terrortylor/nvim-comment",
+        cmd = "CommentToggle",
+        config = function()
+            require("nvim_comment").setup()
+        end
+    }
+    -- Nerdtree and plugins
+    use('preservim/nerdtree')
+    use('ryanoasis/vim-devicons')
+    use('Xuyuanp/nerdtree-git-plugin')
 
-  -- Tabs
-  use('romgrk/barbar.nvim')
-  use('nvim-tree/nvim-web-devicons')
+    -- Tabs
+    use('romgrk/barbar.nvim')
+    use('nvim-tree/nvim-web-devicons')
 
-  use {
-      'rmagatti/auto-session',
-      config = function()
-          require("auto-session").setup {
-              log_level = "error",
-              auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
-          }
-      end
-  }
-  use ('lovesegfault/beautysh')
-  use ('itspriddle/vim-shellcheck')
+    use {
+        'rmagatti/auto-session',
+        config = function()
+            require("auto-session").setup {
+                log_level = "error",
+                auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+            }
+        end
+    }
+    use('lovesegfault/beautysh')
+    use('itspriddle/vim-shellcheck')
 
-  -- Pair brackets
-  use {
-      "windwp/nvim-autopairs",
-      config = function() require("nvim-autopairs").setup {} end
-  }
-  
-  -- use {
-  --     'VonHeikemen/fine-cmdline.nvim',
-  --     requires = {
-  --         {'MunifTanjim/nui.nvim'}
-  --     }
-  -- }
+    -- Pair brackets
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
 end)
