de0f268b813127902b6fa98b073653244ca7e91d
zantpok <anton.pokhodun.ext@ericsson.com>
2024-04-05 14:24:05 +0300

    Added diffview plugin

--- lua/hpkns/packer.lua
+++ lua/hpkns/packer.lua
@@ -121,4 +121,5 @@ return require('packer').startup(function(use)
         "windwp/nvim-autopairs",
         config = function() require("nvim-autopairs").setup {} end
     }
+    use "sindrets/diffview.nvim"
 end)
