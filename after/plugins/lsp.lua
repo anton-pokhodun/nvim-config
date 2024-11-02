0a59f230ba3ca55b7ce13108fdbe275746b06fef
antonpohodun <antony.hopkins008@gmail.com>
2024-11-02 22:03:37 +0200

    Added schemes for yamlls

--- after/plugins/lsp.lua
+++ after/plugins/lsp.lua
@@ -36,7 +36,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end,
 })
 
--- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require('mason').setup({})
 
 require('mason-lspconfig').setup({
@@ -48,14 +47,37 @@ require('mason-lspconfig').setup({
 
 require('lspconfig').dockerls.setup({})
 
-
 require('lspconfig').terraformls.setup({})
 
 require('lspconfig').marksman.setup({})
 
 lspconfig.solargraph.setup({})
 
-lspconfig.yamlls.setup({})
+lspconfig.yamlls.setup({
+
+    settings = {
+        format = {
+            enabled = true
+        },
+        yaml = {
+            schemas = {
+                 kubernetes = "*.yaml",
+                ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+                ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+                ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+                ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+                ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+                ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+                ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+                ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+                ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+                ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+                ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+                ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+            }
+        }
+    }
+})
 
 lspconfig.lua_ls.setup {
     on_attach = lsp_zero.on_attach,
@@ -66,5 +88,4 @@ lspconfig.lua_ls.setup {
             }
         }
     }
-
 }
