a0e9f54d37c461b319165a3bdafa785bf7e0689d
antonpohodun <antony.hopkins008@gmail.com>
2025-01-12 19:44:18 +0200

    Removed fugitive
    For now LazyGit covers all the git-based tasks

--- lua/hpkns/packer.lua
+++ lua/hpkns/packer.lua
@@ -42,7 +42,6 @@ return require('packer').startup(function(use)
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('nvim-treesitter/playground')
     use('mbbill/undotree')
-    use('tpope/vim-fugitive')
     use('vim-airline/vim-airline-themes')
     use {
         'VonHeikemen/lsp-zero.nvim',
